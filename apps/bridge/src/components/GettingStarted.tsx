38a43694520be4eeba294e7a8c6767b991f42007
lebraat <dan.brunsdon@gmail.com>
2023-09-21 14:52:43 -0600

    adjusted language and font size

--- apps/bridge/src/components/GettingStarted.tsx
+++ apps/bridge/src/components/GettingStarted.tsx
@@ -1,16 +1,13 @@
 export const GettingStarted = () => {
   return (
     <>
-    <div className="text-sm">
-      Please note that withdrawling pgnETH to ETH requires a seven day challenge period.
-    </div>
-    <div className="text-sm">
-      For in-depth steps, check out the {" "}
+    <div className="text-base">
       <a
       className="underline"
       target="_blank"
       href="https://docs.publicgoods.network/using-pgn/bridging"
-      >PGN bridging guide</a>.
+      >More info on bridging and withdrawing from PGN
+      </a>
     </div>
     </>
   );
