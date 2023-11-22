577215196a9e177cb8ec6829461708428ad9ca5d
lebraat <dan.brunsdon@gmail.com>
2023-11-22 13:08:49 -0700

    Updated bridge language with other bridge details and links

--- apps/bridge/src/components/GettingStarted.tsx
+++ apps/bridge/src/components/GettingStarted.tsx
@@ -6,8 +6,11 @@ export const GettingStarted = () => {
       className="underline"
       target="_blank"
       href="https://docs.publicgoods.network/using-pgn/bridging"
-      >More details on bridging to and withdrawing from PGN. 
+      >Bridging Options on PGN 
       </a>
+      <p><a className="underline" target="_blank" href="https://bridge.publicgoods.network">PGN native bridge</a>: L1 to L2 depositing and withdrawing of all officially deployed tokens, including DAI, USDT and GTC. There is a 7-day challenge period and process for withdraws back to L1.</p>
+      <p><a className="underline" target="_blank" href="https://www.layerswap.io/app">LayerSwap</a>: L1 to L2 depositing and withdrawing and L2 to L2 bridging for ETH. Withdrawls are not subject to the challenge period and clear in minutes.</p>
+      <p><a className="underline" target="_blank" href="https://app.superbridge.app">Superbridge</a>: L1 to L2 depositing and withdrawing of all officially deployed tokens, including DAI, USDT and GTC. There is a 7 day challenge period for withdraws back to L1, with unique features to make the process smooth.</p>
     </div>
     </>
   );
