dd63c91c0445a7372d46c5543cc688109962c775
sophiahdew <sophiahdew@gmail.com>
2023-12-27 11:59:28 -0800

    renamed to USD Coin

--- packages/react/src/config/tokens.ts
+++ packages/react/src/config/tokens.ts
@@ -92,15 +92,15 @@ export const USDT = {
 };
 
 export const USDC = {
-  name: "USDC",
-  symbol: "USDC",
+  name: "USD Coin",
+  symbol: "USD Coin",
   decimals: 6,
   tokens: {
     mainnet: {
-      address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
     },
     homestead: {
-      address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
     },
     pgnTestnet: {
       address: "",
