c673ccf488f3bf3494fee95472654fb8bea74115
sophiahdew <sophiahdew@gmail.com>
2024-03-13 13:15:30 -1000

    PGN announcement

--- apps/bridge/src/components/GettingStarted.tsx
+++ apps/bridge/src/components/GettingStarted.tsx
@@ -14,6 +14,7 @@ export const GettingStarted = () => {
         >Bridging Options on PGN
         </a></b>
         <ul>
+          <li><p className="paragraph-spacing">NOTE: The Network is winding down in June, after which funds will be unavailable. We strongly recommend that users DO NOT DEPOSIT funds to PGN. For more information: <a className="underline" target="_blank" href="https://gov.gitcoin.co/t/announcing-the-spin-down-of-pgn-eol-june-2024/17430">PGN spin down announcement </a></p></li>
           <li><p className="paragraph-spacing"><a className="underline" target="_blank" href="https://app.rollbridge.app">Rollbridge</a>: L1 to L2 depositing and withdrawing of all officially deployed tokens, including DAI, USDT and GTC. There is a 7 day challenge period for withdraws back to L1, with unique features to make the process smooth.</p></li>
           <li><p className="paragraph-spacing"><a className="underline" target="_blank" href="https://www.layerswap.io/app">LayerSwap</a>: L1 to L2 depositing and withdrawing and L2 to L2 bridging for ETH. Withdrawls are not subject to the challenge period and clear in minutes.</p></li>
           <li><p className="paragraph-spacing"><a className="underline" target="_blank" href="https://bridge.publicgoods.network">PGN native bridge</a>: L1 to L2 depositing and withdrawing of all officially deployed tokens, including DAI, USDT and GTC. There is a 7-day challenge period and process for withdraws back to L1.</p></li>
